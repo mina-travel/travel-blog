a3f04269ec567db11750070797bfdcbd7fd484f1
mina-travel <yamada.potato8@gmail.com>
2026-08-04 17:33:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const posts = [{title: "【香川女子旅】<br>「ご当地グルメ&小豆島
                {title: "【長野一人旅 Day1】<br>「善光寺に栗の町\"小布施\"を巡る」",
                 number: "［旅記録 #2］",
                 date: "2025.08.23〜24",
-                description: "前日に急遽思い立ち、長野へ一人旅<br>素敵な出会いが沢山の旅です。",
+                description: "前日に急遽思い立ち、長野へ一人旅へ。<br>素敵な出会いが沢山の旅です。",
                 image: "images/nagano-19.jpg",
                 url: "travel-2.html"}];
 
